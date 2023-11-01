f7b5526af1e08e3f4738c5fe5ed0387431ee007f
Pham Thien Tan <onlystard@gmail.com>
2023-11-01 10:41:55 +0700

    Initial Commit

---
+++ AirbnbTutorial/AirbnbTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbTutorialApp.swift
+//  AirbnbTutorial
+//
+//  Created by Kay on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
